5b20f032b6e0439b3de34fe6cdd97158fde5d739
Aakash Srivastava <51444354+aakash-thedev@users.noreply.github.com>
2024-08-13 02:43:33 +0530

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const PROD_MONGO_URL="mongodb+srv://aakashsrivastava:Aakash2022@cluster0.rmts6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb+srv://aakashsrivastava:Aakash2022@cluster0.rmts6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect('mongodb://localhost:27017/split-easy');
     console.log("Mongo Atlast Connected");
   } catch (e) {
     console.log("Error connecting to mongodb", e);
